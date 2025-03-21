111e6cba0a9498da316936a2b6eb254a556b0ae6
liangshan <>
2025-03-22 01:47:21 +0800

    no message

--- src/server.ts
+++ src/server.ts
@@ -89,67 +89,67 @@ server.addTool({
   },
 });
 
-server.addTool({
-  name: "getMyJobIntention",
-  description:
-    "获取我的求职意向，包括期望工作地点、期望工作行业、期望工作性质、期望薪资",
-  parameters: z.object({
-    at: z.string().optional(),
-    rt: z.string().optional(),
-  }),
-  execute: async (args) => {
-    if (!args.at || !args.rt) {
-      return {
-        content: [
-          {
-            type: "text",
-            text: `at/rt不能为空，提示用户检查是否登录`,
-          },
-        ],
-      };
-    }
-    const { resumeNumber } = await getResumeNumber({
-      at: args.at,
-      rt: args.rt,
-    });
+// server.addTool({
+//   name: "getMyJobIntention",
+//   description:
+//     "获取我的求职意向，包括期望工作地点、期望工作行业、期望工作性质、期望薪资",
+//   parameters: z.object({
+//     at: z.string().optional(),
+//     rt: z.string().optional(),
+//   }),
+//   execute: async (args) => {
+//     if (!args.at || !args.rt) {
+//       return {
+//         content: [
+//           {
+//             type: "text",
+//             text: `at/rt不能为空，提示用户检查是否登录`,
+//           },
+//         ],
+//       };
+//     }
+//     const { resumeNumber } = await getResumeNumber({
+//       at: args.at,
+//       rt: args.rt,
+//     });
     
-    if (!resumeNumber) {
-      return {
-        content: [
-          {
-            type: "text",
-            text: "简历编号不能为空，提示用户检查是否登录，是否创建过简历",
-          },
-        ],
-      };
-    }
-    const resumeDetail: any = await getResumeDetail({
-      resumeNumber: resumeNumber,
-      at: args.at,
-      rt: args.rt,
-      lang: "1",
-    });
-    if (resumeDetail.UnifiedPurpose && resumeDetail.UnifiedPurpose.length > 0) {
-      return {
-        content: [
-          {
-            type: "text",
-            text: `你的期望工作地点: ${resumeDetail.UnifiedPurpose[0].preferredLocationFirstTranslation}\n期望工作行业: ${resumeDetail.UnifiedPurpose[0].pnewPreferredIndustryTranslation}\n期望工作性质: ${resumeDetail.UnifiedPurpose[0].preferredJobNatureTranslation}\n期望薪资: ${resumeDetail.UnifiedPurpose[0].preferredSalaryMin} 至 ${resumeDetail.UnifiedPurpose[0].preferredSalaryMax} 元/月，用户获取求职意向相关内容时，不要向用户推荐职位，只返回求职意向相关内容。\n如果用户只想获取期望工作地点，期望工作行业，期望工作性质，期望薪资，请只返回期望工作地点，或期望工作行业，或期望工作性质，或期望薪资，不要返回其他内容。\n如果用户想获取求职意向，则返回所有内容`,
-          },
-        ],
-      };
-    } else {
-      return {
-        content: [
-          {
-            type: "text",
-            text: "获取求职意向失败，提示用户检查是否登录，是否创建过简历",
-          },
-        ],
-      };
-    }
-  },
-});
+//     if (!resumeNumber) {
+//       return {
+//         content: [
+//           {
+//             type: "text",
+//             text: "简历编号不能为空，提示用户检查是否登录，是否创建过简历",
+//           },
+//         ],
+//       };
+//     }
+//     const resumeDetail: any = await getResumeDetail({
+//       resumeNumber: resumeNumber,
+//       at: args.at,
+//       rt: args.rt,
+//       lang: "1",
+//     });
+//     if (resumeDetail.UnifiedPurpose && resumeDetail.UnifiedPurpose.length > 0) {
+//       return {
+//         content: [
+//           {
+//             type: "text",
+//             text: `你的期望工作地点: ${resumeDetail.UnifiedPurpose[0].preferredLocationFirstTranslation}\n期望工作行业: ${resumeDetail.UnifiedPurpose[0].pnewPreferredIndustryTranslation}\n期望工作性质: ${resumeDetail.UnifiedPurpose[0].preferredJobNatureTranslation}\n期望薪资: ${resumeDetail.UnifiedPurpose[0].preferredSalaryMin} 至 ${resumeDetail.UnifiedPurpose[0].preferredSalaryMax} 元/月，用户获取求职意向相关内容时，不要向用户推荐职位，只返回求职意向相关内容。\n如果用户只想获取期望工作地点，期望工作行业，期望工作性质，期望薪资，请只返回期望工作地点，或期望工作行业，或期望工作性质，或期望薪资，不要返回其他内容。\n如果用户想获取求职意向，则返回所有内容`,
+//           },
+//         ],
+//       };
+//     } else {
+//       return {
+//         content: [
+//           {
+//             type: "text",
+//             text: "获取求职意向失败，提示用户检查是否登录，是否创建过简历",
+//           },
+//         ],
+//       };
+//     }
+//   },
+// });
 
 server.addTool({
   name: "getMyResumeInfo",
@@ -186,6 +186,7 @@ server.addTool({
       };
     }
 
+    // const jobIntentionInfo = ['期望职位', '期望行业', '期望城市', '期望薪资', '期望工作性质']
     // const profileInfo = ['姓名', '性别', '当前身份', '出生日期', '户口所在地', '现居住地', '政治面貌', '最高学历', '手机号码', '电子邮箱']
     // const educationInfo = ['学校名称', '学历', '专业', '在校时间']
     // const workExperienceInfo = ['公司名称', '所属行业', '职位名称', '拥有技能', '在职时间', '离职时间', '当前月薪', '工作描述或内容']
@@ -199,6 +200,7 @@ server.addTool({
     });
     if (resumeDetail.UnifiedPurpose && resumeDetail.UnifiedPurpose.length > 0) {
       return {
+        // \n求职意向: - 期望职位: ${resumeDetail.UnifiedPurpose.map((item: any) => item.title).join(', ')} - 期望行业: ${resumeDetail.UnifiedPurpose[0].pnewPreferredIndustryTranslation} - 期望城市: ${resumeDetail.UnifiedPurpose.map((item: any) => item.preferredLocationTranslation).join(', ')} - 期望薪资: ${resumeDetail.UnifiedPurpose[0].preferredSalaryMin} 至 ${resumeDetail.UnifiedPurpose[0].preferredSalaryTranslation} - 期望工作性质: ${resumeDetail.UnifiedPurpose[0].preferredJobNatureTranslation}
         content: [
           {
             type: "text",
@@ -213,16 +215,18 @@ server.addTool({
             \n手机号码: ${resumeDetail.Profile[0].mobileBinding}
             \n电子邮箱: ${resumeDetail.Profile[0].email || '未填写'}
             \n求职状态: ${resumeDetail.Profile[0].currentStatusTranslationCN || '未填写'}
+            \n求职意向: ${resumeDetail.UnifiedPurpose.map((item: any) => `期望行业: ${item.pnewPreferredIndustryTranslation} - 期望城市: ${item.preferredLocationTranslation} - 期望薪资: ${item.preferredSalaryMin}-${item.preferredSalaryMax} 元/月 - 期望工作性质: ${item.preferredJobNatureTranslation}`).join('\n')}
             \n教育经历: ${resumeDetail.EducationExperience.map((item: any) => `学校名称: ${item.eduSchoolName} - 学历: ${item.eduBackgroundTranslation} - 专业: ${item.eduMajorV} - 在校时间: ${item.eduStartDateFormat.split(' ')[0]}至${item.eduEndDateFormat.split(' ')[0]}`).join('\n')}
             \n工作/实习经历: ${resumeDetail.WorkExperience.map((item: any) => `公司名称: ${item.companyName} - 所属行业: ${item.wnewIndustryTranslation} - 职位名称: ${item.title} - 拥有技能: ${item.skillTagList.map((skill: any) => skill.name).join(',')} - 当前月薪: ${item.realSalary} 元/月 - 在职时间: ${item.startDateFormat.split(' ')[0]} - 离职时间: ${item.endDateFormat.split(' ')[0]} - 工作描述或内容: ${item.workDesc}`).join('\n')}
             \n项目经历: ${resumeDetail.ProjectExperience.map((item: any) => `项目名称: ${item.proExpProjectName} - 项目开始时间: ${item.proExpStartDateFormat.split(' ')[0]} - 项目结束时间 ${item.proExpEndDateFormat.split(' ')[0]} - 项目描述: ${item.proExpProjectDesc}`).join('\n')}
             \n如果用户只想获取姓名，请只返回用户的姓名，不要返回其他内容。
+            \n如果用户只想获取求职意向中的期望行业，则只返回求职意向中的期望行业，不要返回其他内容，并输出为表格形式，表格使用 HTML 标签。
             \n如果用户只想获取教育经历中的某一项，如教育经历中的学校名称，则只返回教育经历的学校名称，不要返回其他内容，并输出为表格形式，表格使用 HTML 标签。
             \n如果用户只想获取工作/实习经历的公司名称，则只返回工作/实习经历中的公司名称，不要返回其他内容，并输出为表格形式，表格使用 HTML 标签。
             \n如果用户询问“是否在XX公司工作过”，则从所有工作/实习的公司名称中，查找是否包含XX公司，如果包含，则只返回在XX公司的工作/实习经历，不返回其他公司的工作/实习经历，并输出为表格形式，表格使用 HTML 标签；否则返回“否”，不要返回其他内容。
             \n如果用户只想获取项目经历的项目名称，则只返回项目经历中的项目名称，不要返回其他内容，并输出为表格形式，表格使用 HTML 标签。
-            \n如果用户只想获取某一份教育经历，或工作/实习经历，或项目经历，则只返回该份教育经历，或工作/实习经历，或项目经历，不要返回其他内容，并输出为表格形式，表格使用 HTML 标签。
-            \n如果用户想获取所有教育经历的学校，或工作/实习经历的公司，或项目经历的项目，则返回所有教育经历的学校，或工作/实习经历的公司，或项目经历的项目，并输出为表格形式，表格使用 HTML 标签。`,
+            \n如果用户只想获取某一份教育经历，或工作/实习经历，或项目经历，或求职意向，则只返回该份教育经历，或工作/实习经历，或项目经历，或求职意向，不要返回其他内容，并输出为表格形式，表格使用 HTML 标签。
+            \n如果用户想获取所有教育经历的学校，或工作/实习经历的公司，或项目经历的项目，或求职意向的期望行业，则返回所有教育经历的学校，或工作/实习经历的公司，或项目经历的项目，或求职意向的期望行业，并输出为表格形式，表格使用 HTML 标签。`,
           },
         ],
       };
